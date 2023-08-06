a8d806c299248cd0f9442c66cbc230034a8cbedc
sheldon <sheldonmwangi254@gmail.com>
2023-08-07 01:35:15 +0300

    completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,8 @@
+
+
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ handleAddNewQuestion }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -17,9 +19,28 @@ function QuestionForm(props) {
     });
   }
 
+  //POST method
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((newQuestion) => handleAddNewQuestion(newQuestion));
   }
 
   return (
